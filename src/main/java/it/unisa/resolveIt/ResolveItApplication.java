efcc1bccab18b029221bb665ec97fe5ad779158d
samueleNacchia <samu.nacchia@gmail.com>
2025-12-22 18:06:29 +0100

    Inizializzazione progetto

---
+++ src/main/java/it/unisa/resolveIt/ResolveItApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.resolveIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolveItApplication.class, args);
+	}
+
+}
