cc1621c0602e9c9c728ed4463771cb505fd46cad
MarioDiFeo <m.difeo20@studenti.unisa.it>
2025-12-28 14:50:57 +0100

    Aggiunti: package account, contenente controller e service, package categoria, controller e service, entity. Aggiunto entity Categoria Aggiunto CategoriaRepository Rimosso CategoriaStub Modificato Ticket per utilizzare Categoria invece di CategoriaStub

---
+++ src/main/java/it/unisa/resolveIt/model/repository/CategoriaRepository.java
@@ -0,0 +1,18 @@
+package it.unisa.resolveIt.model.repository;
+
+import it.unisa.resolveIt.model.enums.StatoCategoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import it.unisa.resolveIt.model.entity.Categoria;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    @Override
+    List<Categoria> findAll();
+    Categoria findByNome(String nome);
+    List<Categoria> findAllByStato(StatoCategoria stato);
+    @Override
+    Optional<Categoria> findById(Long id);
+}
